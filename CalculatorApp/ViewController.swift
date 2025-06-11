02785278c693e6549839219b95b5ed97aac45267
Mariam Joglidze <mariamjoglidze1@gmail.com>
2025-06-12 01:00:35 +0400

    first commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
